fabc5a9e15c31a89bf5b4d07f7cf8be2c23609f2
Cédric Pontet <cedric.pontet@gmail.com>
2021-05-02 09:48:16 +0200

    Implemented dynamic event list

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
 import Layout from "../components/App/Layout"
 import Navbar from "../components/App/Navbar"
 import PageBanner from '../components/Common/PageBanner'
 import Footer from "../components/App/Footer"
 import EventsCard from '../components/Events/EventsCard'
 
+export const query = graphql`
+query GetEvents {
+    events: allStrapiEvent( sort: {fields:start, order: DESC} ) {
+      nodes {
+        slug
+        name
+        date
+        location {
+          name
+        }
+        venue {
+          name
+        }
+        status
+      }
+    }
+  }
+`
+
 const Events = () => {
+    const data = useStaticQuery(query)
     return (
         <Layout>
             <Navbar />
@@ -15,7 +36,7 @@ const Events = () => {
                 homePageUrl="/" 
                 activePageText="Events" 
             />
-            <EventsCard />
+            <EventsCard events={data.events.nodes} />
             <Footer />
         </Layout>
     );
