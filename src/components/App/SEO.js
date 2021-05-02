6b3ad31c2deaed1eed1571163229bae39a4456b7
Cédric Pontet <cedric.pontet@gmail.com>
2021-05-02 09:47:08 +0200

    Changed SEO

--- src/components/App/SEO.js
+++ src/components/App/SEO.js
@@ -5,12 +5,12 @@ const SEO = () => {
     return (
         <div>
             <Helmet>
-                <title>Rewy - Gatsby IT Startup Templates Collection</title>
-                <meta name="description" content="Rewy - React Gatsby IT Startup Templates Collection" />
-                <meta name="og:title" property="og:title" content="Rewy - React Gatsby IT Startup Templates Collection"></meta>
-                <meta name="twitter:card" content="Rewy - React Gatsby IT Startup Templates Collection"></meta>
+                <title>#play14 - We believe in playfulness</title>
+                <meta name="description" content="#play14 - We believe in playfulness" />
+                <meta name="og:title" property="og:title" content="#play14 - We believe in playfulness"></meta>
+                <meta name="twitter:card" content="#play14 - We believe in playfulness"></meta>
                 <link rel="canonical" href="https://rewy-react.envytheme.com/"></link>
-                <meta property="og:image" content="https://res.cloudinary.com/dev-empty/image/upload/v1593069801/explore-learning.jpg" />
+                <meta property="og:image" content="https://play14.org/images/logo.png" />
             </Helmet>
         </div>
     )
