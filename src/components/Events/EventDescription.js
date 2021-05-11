e5c4ed550258622938c5525d14f9dd70c0586615
Cédric Pontet <cedric.pontet@gmail.com>
2021-05-11 23:30:51 +0200

    Coped with null venue

--- src/components/Events/EventDescription.js
+++ src/components/Events/EventDescription.js
@@ -7,7 +7,6 @@ import EventDate from './EventDate'
 
 const EventDescription = (props) => {
     const { event } = props
-    const { number, street, postalCode, city, country } = event.venue.address
 
     return (
         <section className="events-details-area pb-100">
@@ -21,21 +20,35 @@ const EventDescription = (props) => {
                                 <li><i className='bx bx-time'></i>Starts at <Moment format="HH:mm on MMM. DD">{event.start}</Moment></li>
                                 <li><i className='bx bx-time-five'></i>Ends at <Moment format="HH:mm on MMM. DD">{event.end}</Moment></li>
                             </ul>
-                            <ul>
-                                <li><i className='bx bx-building'></i>{event.venue.name}</li>
+                            { event.venue ? 
+                                <ul>
+                                    <li><i className='bx bx-building'></i>{event.venue.name}</li>
                                 </ul>
-                            <ul>
-                                <li><i className='bx bx-map'></i>{number} {street}, {postalCode} {city}, {country} </li>
-                            </ul>
+                                : ''
+                            }
+                            { event.venue && event.venue.address ? 
+                                <ul>
+                                    <li>
+                                        <i className='bx bx-map'></i>
+                                        {event.venue.address.number} {event.venue.address.street}, 
+                                        {event.venue.address.postalCode} {event.venue.address.city}, {event.venue.address.country} 
+                                    </li>
+                                </ul>
+                                : ''
+                            }
                         </div>
-
-                        <div className="events-details-location">
-                            <iframe src={event.venue.embeddedMapUrl}></iframe>
-                        </div>
-
-                        <div className="events-details-desc">
-                            <Markdown>{event.description}</Markdown>
-                        </div>
+                        { event.venue ?
+                            <div className="events-details-location">
+                                <iframe src={event.venue.embeddedMapUrl}></iframe>
+                            </div>
+                          : ''
+                        }
+                        { event.description ?
+                            <div className="events-details-desc">
+                                <Markdown>{event.description}</Markdown>
+                            </div>
+                          : ''
+                        }
                     </div>
                     <div className="col-lg-4 col-md-12">
                         <EventSidebar event={event} />
